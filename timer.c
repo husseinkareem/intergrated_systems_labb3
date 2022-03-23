c364b8325143771f3d44ab7dd19b73dc4cd08202
husseinkareem <husseinkeldoon@yahoo.com>
2022-03-23 13:57:42 +0100

    del3 timer2

--- timer.c
+++ timer.c
@@ -29,8 +29,8 @@ void timer2() {
     TCCR2B &= ~(1 << WGM22);
 
     // 1 1 Set OC2A on compare match
-    TCCR2A |= (1 << COM2A0);
+    TCCR2A &= ~(1 << COM2A0);
     TCCR2A |= (1 << COM2A1);
 
-    OCR2A = 15;  // Set top counter to 16000000/1024*0.001 -1 since it starts at index 0, to get ~1ms
+    OCR2A = 156;  // Set top counter to 16000000/1024*0.01 -1 since it starts at index 0, to get ~1ms
 }
